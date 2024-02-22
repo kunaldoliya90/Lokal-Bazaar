863c23db31a9c32358cecd772b18c9a314a69461
kunaldoliya90 <kunal.doliya90@gmail.com>
2024-02-22 11:56:11 +0530

    Theme Utils

---
+++ lib/utils/theme/custom_themes/text_theme.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class TTextTheme {
+  TTextTheme._();
+
+  static TextTheme lightTextTheme = TextTheme(
+    headlineLarge: TextStyle().copyWith(
+        fontSize: 32.0, fontWeight: FontWeight.bold, color: Colors.black),
+    headlineMedium: TextStyle().copyWith(
+        fontSize: 24.0, fontWeight: FontWeight.w600, color: Colors.black),
+    headlineSmall: TextStyle().copyWith(
+        fontSize: 18.0, fontWeight: FontWeight.w600, color: Colors.black),
+    titleLarge: TextStyle().copyWith(
+        fontSize: 16.0, fontWeight: FontWeight.w600, color: Colors.black),
+    titleMedium: TextStyle().copyWith(
+        fontSize: 16.0, fontWeight: FontWeight.w500, color: Colors.black),
+    titleSmall: TextStyle().copyWith(
+        fontSize: 16.0, fontWeight: FontWeight.w400, color: Colors.black),
+    bodyLarge: TextStyle().copyWith(
+        fontSize: 32.0, fontWeight: FontWeight.bold, color: Colors.black),
+    bodyMedium: TextStyle().copyWith(
+        fontSize: 24.0, fontWeight: FontWeight.w600, color: Colors.black),
+    bodySmall: TextStyle().copyWith(
+        fontSize: 18.0, fontWeight: FontWeight.w600, color: Colors.black),
+    labelLarge: TextStyle().copyWith(
+        fontSize: 12.0, fontWeight: FontWeight.normal, color: Colors.black),
+    labelMedium: TextStyle().copyWith(
+        fontSize: 12.0,
+        fontWeight: FontWeight.normal,
+        color: Colors.black.withOpacity(0.5)),
+  );
+  static TextTheme darkTextTheme = TextTheme(
+    headlineLarge: TextStyle().copyWith(
+        fontSize: 32.0, fontWeight: FontWeight.bold, color: Colors.white),
+    headlineMedium: TextStyle().copyWith(
+        fontSize: 24.0, fontWeight: FontWeight.w600, color: Colors.white),
+    headlineSmall: TextStyle().copyWith(
+        fontSize: 18.0, fontWeight: FontWeight.w600, color: Colors.white),
+  );
+}
---
+++ lib/utils/theme/theme.dart
@@ -0,0 +1,32 @@
+import 'package:ecomapp/utils/theme/custom_themes/text_theme.dart';
+import 'package:flutter/material.dart';
+
+class TAppTheme {
+  TAppTheme._();
+
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: TTextTheme.lightTextTheme,
+  );
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.dark,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.black,
+    textTheme: TTextTheme.darkTextTheme,
+  );
+}
+
+
+/* 
+
+TextTheme(
+              bodyLarge: TextStyle(fontSize: 14, color: Colors.green))),
+      darkTheme: ThemeData.dark(),
+
+*/
