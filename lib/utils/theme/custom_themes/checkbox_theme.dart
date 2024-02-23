756d660eab174cf96af1f6ccfcedd5ef5200f12d
kunaldoliya90 <kunal.doliya90@gmail.com>
2024-02-23 15:00:55 +0530

    commit

---
+++ lib/utils/theme/custom_themes/checkbox_theme.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class TCheckboxTheme {
+  TCheckboxTheme._(); // To avoid creating instances
+  /// Customizable Light Text Theme
+  static CheckboxThemeData LightCheckboxTheme = CheckboxThemeData(
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+    checkColor: MaterialStateProperty.resolveWith((states) {
+      if (states.contains(MaterialState.selected)) {
+        return Colors.white;
+      } else {
+        return Colors.black;
+      }
+    }),
+    fillColor: MaterialStateProperty.resolveWith((states) {
+      if (states.contains(MaterialState.selected)) {
+        return Colors.blue;
+      } else {
+        return Colors.transparent;
+      }
+    }),
+  );
+
+  static CheckboxThemeData DarkCheckboxTheme = CheckboxThemeData(
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+    checkColor: MaterialStateProperty.resolveWith((states) {
+      if (states.contains(MaterialState.selected)) {
+        return Colors.white;
+      } else {
+        return Colors.black;
+      }
+    }),
+    fillColor: MaterialStateProperty.resolveWith((states) {
+      if (states.contains(MaterialState.selected)) {
+        return Colors.blue;
+      } else {
+        return Colors.transparent;
+      }
+    }),
+  );
+}
