9d5617c05d32d46bc3785d1df36c0a6a784fbafc
kunaldoliya90 <kunal.doliya90@gmail.com>
2024-02-26 21:31:59 +0530

    Comments Added

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      //Here Continue--------
+      //Here Continue
       themeMode: ThemeMode.system,
       theme: TAppTheme.lightTheme,
       darkTheme: TAppTheme.darkTheme,
