63cc50d7b9d1227d6c1c263c5b9869a436ddbfeb
kunaldoliya90 <kunal.doliya90@gmail.com>
2024-03-01 11:14:03 +0530

    constant colors added

--- lib/utils/constants/colors.dart
+++ lib/utils/constants/colors.dart
@@ -29,4 +29,24 @@ class TColors {
   static Color darkContainer = Colors.white.withOpacity(0.1);
 
   // Button Colors
+  static const Color buttonPrimary = Color(0xFF4b68ff);
+  static const Color buttonSecondary = Color(0xFF6C757D);
+  static const Color buttonDisabled = Color(0xFFC4C4C4);
+// Border Colors
+  static const Color borderPrimary = Color(0xFFD9D909);
+  static const Color borderSecondary = Color(0xFFE6E6E6);
+// Error and Validation Colors
+  static const Color error = Color(0xFFD32F2F);
+  static const Color success = Color(0xFF388E3C);
+  static const Color warning = Color(0xFFF57000);
+  static const Color info = Color(0xFF197602);
+
+// Neutral Shades
+  static const Color black = Color(0xFF232323);
+  static const Color darkerGrey = Color(0xFF4F4F4F);
+  static const Color darkGrey = Color(0xFF939393);
+  static const Color grey = Color(0xFFE0E0E0);
+  static const Color softGrey = Color(0xFFF4F4F4);
+  static const Color lightGrey = Color(0xFFF9F9F9);
+  static const Color white = Color(0xFFFFFFFF);
 }
