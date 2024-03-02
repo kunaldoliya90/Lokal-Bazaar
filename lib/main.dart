5baa9127aff4b8c71215401201041ffe93f4a73c
kunaldoliya90 <kunal.doliya90@gmail.com>
2024-03-02 11:10:51 +0530

    added some utility files

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:ecomapp/app.dart';
-import 'package:ecomapp/utils/constants/colors.dart';
-import 'package:ecomapp/utils/theme/theme.dart';
+// import 'package:ecomapp/utils/constants/colors.dart';
+// import 'package:ecomapp/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-
-  
 
   runApp(const App());
 }
---
+++ lib/utils/helpers/helper_function.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+
+class THelperFunctions {
+  static Color? getColor(String value) {
+    if (value == 'Green') {
+      return Colors.green;
+    } else if (value == 'Red') {
+      return Colors.red;
+    } else if (value == 'Blue') {
+      return Colors.blue;
+    } else if (value == 'Pink') {
+      return Colors.pink;
+    } else if (value == 'Grey') {
+      return Colors.grey;
+    } else if (value == 'Purple') {
+      return Colors.purple;
+    } else if (value == 'Black') {
+      return Colors.black;
+    } else if (value == 'White') {
+      return Colors.white;
+    } else if (value == 'Brown') {
+      return Colors.brown;
+    } else if (value == 'Indigo') {
+      return Colors.indigo;
+    } else if (value == 'Teal') {
+      return Colors.teal;
+    } else {
+      return null; // Return null for values that don't match any color
+    }
+  }
+
+  static void showSnackBar(String message) {
+    ScaffoldMessenger.of(Get.context!).showSnackBar(
+      SnackBar(content: Text(message)),
+    );
+  }
+
+  static void showAlert(String title, String message) {
+    showDialog(
+      context: Get.context!,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(message),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  static void navigateToScreen(BuildContext context, Widget screen) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => screen),
+    );
+  }
+
+  static String truncateText(String text, int maxLength) {
+    if (text.length <= maxLength) {
+      return text;
+    } else {
+      return '${text.substring(0, maxLength)}...';
+    }
+  }
+
+  static bool isDarkMode(BuildContext context) {
+    return Theme.of(context).brightness == Brightness.dark;
+  }
+
+  static Size screenSize() {
+    return MediaQuery.of(Get.context!).size;
+  }
+
+  static double screenHeight() {
+    return MediaQuery.of(Get.context!).size.height;
+  }
+
+  static String getFormattedDate(DateTime date,
+      {String format = 'dd MMM yyyy'}) {
+    return DateFormat(format).format(date);
+  }
+
+  static List<T> removeDuplicates<T>(List<T> list) {
+    return list.toSet().toList();
+  }
+
+  static List<Widget> wrapWidgets(List<Widget> widgets, int rowSize) {
+    final wrappedList = <Widget>[];
+    for (var i = 0; i < widgets.length; i += rowSize) {
+      final rowChildren = widgets.sublist(
+          i, i + rowSize < widgets.length ? i + rowSize : widgets.length);
+      wrappedList.add(Row(children: rowChildren));
+    }
+    return wrappedList;
+  }
+
+
+  
+}
