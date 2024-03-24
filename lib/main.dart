96dc71cf687a2e6ed096948cd1e3cc3ac98fe53a
kunaldoliya90 <112731099+kunaldoliya90@users.noreply.github.com>
2024-03-24 23:33:02 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'firebase_options.dart';
 // ...
 
 void main() async {
+//adding comments
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
